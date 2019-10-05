802890a0c1b4ab92cfa7e6c7e177b6e814a82833
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-10-05 22:57:53 +0900

    const Job applied

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,10 +1,18 @@
 import React  from "react";
 import Layout from "../components/layout"
+import styles from "../css/profile.css"
 import "../css/main.css"
+
+const Job = props => (
+    <div className={styles.company}>
+        aaa
+    </div>
+)
 
 const IndexPage = () => (
     <Layout>
         <h2 id="second_title">Profile</h2>
+        <Job />
     </Layout>
 )
 
